bba7695d40a5e5ce6e1877fa1e7da62f6421615d
Vince Swu <vinceswu88@gmail.com>
2024-09-04 14:42:24 +0530

    update

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 # Blossom
 from dflow import Dflow
+import subprocess
 
 class Core:
     def __init__(self, name):
         self.name = name
         self.agent = Dflow(self.name)
 
+    def speak(self, text):
+        subprocess.run(['espeak', '-ven+f3', '-s 150', '-p 50', text])
+        print(text)
+
     def run(self):
         while True:
-            print(self.agent.get_response(input("Query: ").lower()))
+            self.speak(self.agent.get_response(input("Query: ").lower()))
 
 if __name__ == '__main__':
     core = Core('Blossom')
