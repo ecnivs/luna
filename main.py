3dc61fd7a7d12ac929e7238e85418d5f7b2e40d8
Vince Swu <vinceswu88@gmail.com>
2024-09-10 12:56:56 +0530

    update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pyaudio
 import json
 import threading
 from vosk import Model, KaldiRecognizer
+import time
 
 class Core:
     def __init__(self, name):
@@ -16,8 +17,9 @@ class Core:
         self.recognizer = KaldiRecognizer(self.model, 16000) # 16 KHz sampling rate
         self.query = None # shared variable to store recognized speech
         self.speech_thread = None
-        self.fallback_intent = "Default Fallback Intent"
-        self.last_intent = self.fallback_intent
+        self.called = False
+        self.call_words = ["hey", "okay", "hi", "yo", "listen", "attention", "are you there"] # define call words
+        self.lock = threading.Lock()
 
     def load_vosk_model(self):
         if not os.path.exists(self.model_path):
@@ -41,33 +43,44 @@ class Core:
             if self.recognizer.AcceptWaveform(data):
                 result = json.loads(self.recognizer.Result())
                 if 'text' in result and result['text'].strip() != "":
-                    self.query = result['text'].strip() # update shared variable
+                    with self.lock:
+                        self.query = result['text'].strip() # update shared variable
                     print(f'Recognized: {self.query}')
 
-    def start_sr_thread(self):
+    def get_call(self):
+        while True:
+            with self.lock:
+                if self.query and all([self.name.lower() in self.query.lower(), 
+                                   any(word in self.query.lower() for word in self.call_words)]):
+                    self.called = True
+                    print("call detected!")
+                    self.query = None
+
+    def start_threads(self):
         # start speech recognition in a seperate thread
         self.speech_thread = threading.Thread(target=self.recognize_speech)
         self.speech_thread.daemon = True # ensures thread stops when main program exits
         self.speech_thread.start()
 
+        self.call_thread = threading.Thread(target=self.get_call)
+        self.call_thread.daemon = True # ensures thread stops when main program exits
+        self.call_thread.start()
+
     def run(self):
-        self.start_sr_thread() # start recognizing speech in background
+        self.start_threads()
 
         while True:
-            if self.query: # check if new query is available
-                # process new conversations only if it contains the assistant's name
-                if self.name.lower() in self.query.lower() or self.fallback_intent not in self.last_intent:
-                    print("processing...")
-                    if not self.query == self.name:
-                        self.query = self.query.lower().replace(self.name, "").strip()
-                    self.speak(self.agent.get_response(self.query))
-                    self.last_intent = self.agent.detected_intent
-
-                    # for testing
-                    print(f'{self.agent.response.query_result.intent_detection_confidence} {self.agent.response.query_result.intent.display_name}')
-
-            # reset the query so it doesn't get processed multiple times
-            self.query = None
+            if self.called:
+                with self.lock:
+                    if self.query:
+                        print("processing...")
+                        self.speak(self.agent.get_response(self.query))
+                        print(f'{self.agent.response.query_result.intent_detection_confidence} '
+                            f'{self.agent.response.query_result.intent.display_name}')
+                        self.called = False # reset call flag
+                    self.query = None
+            
+            time.sleep(0.1) # slight sleep to avoid CPU overload
 
 if __name__ == '__main__':
     core = Core('Blossom')
