93e4ff8bfdf223083f561212ea82f62d355e6721
Vince Swu <vinceswu88@gmail.com>
2024-09-16 15:51:55 +0530

    update

--- res_handler.py
+++ res_handler.py
@@ -1,19 +1,26 @@
 # response handler
 from web_handler import Web
 from dflow_handler import Agent
+import spacy
 
 class ResponseHandler:
     def __init__(self, core):
         self.web = Web()
         self.agent = Agent()
         self.core = core
+        self.nlp = spacy.load("en_core_web_sm")
+
+    def extract_key_phrases(self, query):
+        doc = self.nlp(query)
+        phrases = [chunk.text for chunk in doc.noun_chunks if any(token.pos_ in ['PROPN', 'NOUN'] for token in chunk)]
+        return phrases
 
     def handle(self, query):
         self.agent.get_response(query)
         if 'web.search' in self.agent.detected_intent:
-            try:
-                _, query = query.lower().split(self.core.name.lower(), 1)
-            finally:
-                return self.web.search(query)
+            #try:
+            #    _, query = query.lower().split(self.core.name.lower(), 1)
+            #finally:
+            return self.web.search(self.extract_key_phrases(query))
         else:
             return self.agent.fulfillment_text
