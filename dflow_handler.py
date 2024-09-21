25800f40b18f6a33c5fc34e7a5e488011291394d
Vince Swu <vinceswu88@gmail.com>
2024-09-21 20:34:25 +0530

    update

--- dflow_handler.py
+++ dflow_handler.py
@@ -2,35 +2,17 @@ import os
 import google.cloud.dialogflow as dialogflow
 from google.api_core.exceptions import DeadlineExceeded, InvalidArgument
 import uuid
-import socket
 
 class Agent:
     def __init__(self):
-        self.connected = False
-        self.initialize()
-
-    def initialize(self):
-        if self.check_internet:
-            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'key.json'
-            self.project_id = 'blossom-jwv9'
-            self.language = 'en'
-            self.session_id = str(uuid.uuid4())
-            self.connected = None
-            self.session_client = dialogflow.SessionsClient()
-            self.session = self.session_client.session_path(self.project_id, self.session_id)
-            self.connected = True
-
-    def check_internet(self):
-        try:
-            # Try to connect to Google's DNS server
-            socket.create_connection(("8.8.8.8", 53))
-            return True
-        except OSError:
-            return False
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'key.json'
+        self.project_id = 'blossom-jwv9'
+        self.language = 'en'
+        self.session_id = str(uuid.uuid4())
+        self.session_client = dialogflow.SessionsClient()
+        self.session = self.session_client.session_path(self.project_id, self.session_id)
 
     def get_response(self, query, timeout=5):
-        if not self.connected:
-            self.initialize()
         self.text_input = dialogflow.TextInput(text=query, language_code=self.language)
         self.query_input = dialogflow.QueryInput(text=self.text_input)
         try:
@@ -42,9 +24,6 @@ class Agent:
             return "DeadlineExceeded"
         except InvalidArgument:
             return "InvalidArgument"
-        except Exception as e:
-            return f"Unexpected Error: {e}"
-
         return None
         
     def update(self):
