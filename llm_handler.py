d0568d81945fcfa35ff514f3169197c5060f6a53
Vince Swu <vinceswu88@gmail.com>
2025-01-13 17:21:27 +0530

    removed unused function

--- llm_handler.py
+++ llm_handler.py
@@ -9,7 +9,6 @@ logging.basicConfig(level=logging.DEBUG,
 class LlmHandler:
     def __init__(self, core):
         self.name = core.name
-        self.context = []
         self.get_prompt()
 
     def get_prompt(self):
@@ -21,9 +20,6 @@ class LlmHandler:
             with open('.prompt.txt', 'w') as file:
                 file.write(default)
             logging.error("'.prompt.txt' not found. Creating new one.")
-
-    def add_to_context(self, role, content):
-        self.context.append({"role": role, "content": content})
 
     def get_response(self, query):
         data = {
